112e3e761b2e7eb639f0e3dfab082d243fe001df
Mark Lee <octoaudit@lazymalevolence.com>
2014-08-30 19:59:02 -0700

    Properly show amo-validator where spidermonkey is on Travis CI

--- build_tools/validate-xpi.sh
+++ build_tools/validate-xpi.sh
@@ -2,5 +2,10 @@
 
 BASE_DIR="$(dirname $0)"
 
+if [[ -n "$TRAVIS" ]]; then
+    cd "$BASE_DIR"
+    export SPIDERMONKEY_INSTALLATION="$(pwd)/spidermonkey"
+fi
+
 cd "$BASE_DIR"/amo-validator
 ./amo-validator --determined ../../dist/xpi/octoaudit.xpi
