ab07f76c397beb46ca599e646dbbf0dda19efbf0
Mark Lee <octoaudit@lazymalevolence.com>
2014-08-30 19:53:31 -0700

    Another stupid typo

--- build_tools/install-amo-validator.sh
+++ build_tools/install-amo-validator.sh
@@ -4,4 +4,4 @@ BASE_DIR="$(dirname $0)"
 
 cd "$BASE_DIR"
 git clone https://github.com/mozilla/amo-validator.git
-pip install amo-validator/requirements.txt
+pip install -r amo-validator/requirements.txt
