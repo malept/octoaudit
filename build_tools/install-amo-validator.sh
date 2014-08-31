f8fe16ceb5fa7804b0377c93aa6e8d3978ebb0a5
Mark Lee <octoaudit@lazymalevolence.com>
2014-08-30 20:16:10 -0700

    Travis CI: use virtualenv after all to avoid sudo

--- build_tools/install-amo-validator.sh
+++ build_tools/install-amo-validator.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 BASE_DIR="$(dirname $0)"
+VENV_DIR=amov_venv
 
 cd "$BASE_DIR"
-git clone https://github.com/mozilla/amo-validator.git
+
+if [[ -n "$INSTALL_VIRTUALENV" ]]; then
+    VENV_VERSION=1.11.6
+    VENV_NAME=virtualenv-$VENV_VERSION
+    VENV_TGZ=${VENV_NAME}.tar.gz
+    if [[ ! -d "$VENV_DIR" ]]; then
+        if [[ ! -d "$VENV_NAME" ]]; then
+            if [[ ! -f "$VENV_TGZ" ]]; then
+                wget https://pypi.python.org/packages/source/v/virtualenv/$VENV_TGZ
+            fi
+            tar xf $VENV_TGZ
+        fi
+        $VENV_NAME/virtualenv.py "$VENV_DIR"
+    fi
+    source "$VENV_DIR"/bin/activate
+fi
+
+if [[ -d amo-validator ]]; then
+    (cd amo-validator && git pull)
+else
+    git clone https://github.com/mozilla/amo-validator.git
+fi
 pip install -r amo-validator/requirements.txt
