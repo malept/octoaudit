27821382cca206ea5386e048c8fc1ae4c08896d7
Mark Lee <octoaudit@lazymalevolence.com>
2014-08-30 20:19:50 -0700

    validate xpi: fix a few things

--- build_tools/validate-xpi.sh
+++ build_tools/validate-xpi.sh
@@ -6,8 +6,9 @@ if [[ -n "$TRAVIS" ]]; then
     cd "$BASE_DIR"
     export SPIDERMONKEY_INSTALLATION="$(pwd)/spidermonkey"
     cd amo-validator
+    source amov_venv/bin/activate
 else
     cd "$BASE_DIR"/amo-validator
 fi
 
-./amo-validator --determined ../../dist/xpi/octoaudit.xpi
+./addon-validator --determined ../../dist/xpi/octoaudit.xpi
