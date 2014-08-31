de6fb654fcb042af60609c7520555d17e8a00edb
Mark Lee <octoaudit@lazymalevolence.com>
2014-08-30 20:26:24 -0700

    validate xpi: wrong order of commands

--- build_tools/validate-xpi.sh
+++ build_tools/validate-xpi.sh
@@ -5,8 +5,8 @@ BASE_DIR="$(dirname $0)"
 if [[ -n "$TRAVIS" ]]; then
     cd "$BASE_DIR"
     export SPIDERMONKEY_INSTALLATION="$(pwd)/spidermonkey"
-    cd amo-validator
     source amov_venv/bin/activate
+    cd amo-validator
 else
     cd "$BASE_DIR"/amo-validator
 fi
