2d0eade65ffb87066cd8e70689a04101486a4ea1
Mark Lee <octoaudit@lazymalevolence.com>
2014-08-30 20:30:48 -0700

    Travis CI: try to get addon-validator to recognize spidermonkey

--- build_tools/validate-xpi.sh
+++ build_tools/validate-xpi.sh
@@ -4,7 +4,6 @@ BASE_DIR="$(dirname $0)"
 
 if [[ -n "$TRAVIS" ]]; then
     cd "$BASE_DIR"
-    export SPIDERMONKEY_INSTALLATION="$(pwd)/spidermonkey"
     source amov_venv/bin/activate
     cd amo-validator
 else
