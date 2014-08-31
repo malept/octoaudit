52097ab50a9e164de433fdedb326c2b24547a57b
Mark Lee <octoaudit@lazymalevolence.com>
2014-08-30 20:02:25 -0700

    validate xpi: Fix directory navigation

--- build_tools/validate-xpi.sh
+++ build_tools/validate-xpi.sh
@@ -5,7 +5,9 @@ BASE_DIR="$(dirname $0)"
 if [[ -n "$TRAVIS" ]]; then
     cd "$BASE_DIR"
     export SPIDERMONKEY_INSTALLATION="$(pwd)/spidermonkey"
+    cd amo-validator
+else
+    cd "$BASE_DIR"/amo-validator
 fi
 
-cd "$BASE_DIR"/amo-validator
 ./amo-validator --determined ../../dist/xpi/octoaudit.xpi
