50e1f3dcd88d928c991b8a5a1b85d797e27aa54b
Mark Lee <octoaudit@lazymalevolence.com>
2014-12-31 17:04:03 -0800

    Travis: account for cache directory creation

--- build_tools/install-amo-validator.sh
+++ build_tools/install-amo-validator.sh
@@ -9,7 +9,7 @@ if [[ -n "$INSTALL_VIRTUALENV" ]]; then
     VENV_VERSION=1.11.6
     VENV_NAME=virtualenv-$VENV_VERSION
     VENV_TGZ=${VENV_NAME}.tar.gz
-    if [[ ! -d "$VENV_DIR" ]]; then
+    if [[ ! -d "$VENV_DIR" ]] || [[ ! -d "$VENV_DIR/bin" ]]; then
         if [[ ! -d "$VENV_NAME" ]]; then
             if [[ ! -f "$VENV_TGZ" ]]; then
                 wget https://pypi.python.org/packages/source/v/virtualenv/$VENV_TGZ
